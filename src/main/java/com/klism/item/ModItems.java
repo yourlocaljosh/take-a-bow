5010d641b97db5153912e70a2d1454bf5cecce4a
Josh <josh.seungwon.hong@gmail.com>
2025-11-19 01:17:18 -0500

    Correct register syntax with 1.21.10 rules

--- src/main/java/com/klism/item/ModItems.java
+++ src/main/java/com/klism/item/ModItems.java
@@ -6,15 +6,26 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
 
 public class ModItems{
 
-    public static final Item SHORTBOW = Registry.register(
-            Registries.ITEM,
-            Identifier.of(Takeabow.MOD_ID, "shortbow"),
-            new ShortBowItem(new Item.Settings().maxDamage(384))
+    public static final Item SHORTBOW = register(
+            "shortbow",
+            ShortBowItem::new,
+            new Item.Settings().maxDamage(384)
     );
+
+    private static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Takeabow.MOD_ID, name));
+        Item item = itemFactory.apply(settings.registryKey(key));
+        Registry.register(Registries.ITEM, key, item);
+        return item;
+    }
 
     public static void registerModItems(){
         Takeabow.LOGGER.info("Registering items for " + Takeabow.MOD_ID);
