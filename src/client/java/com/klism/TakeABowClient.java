445827a7feca62ba22d286e723de656792558111
Josh <josh.seungwon.hong@gmail.com>
2025-11-19 21:37:14 -0500

    Initialize client class

--- src/client/java/com/klism/TakeABowClient.java
+++ src/client/java/com/klism/TakeABowClient.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class TakeabowClient implements ClientModInitializer{
 	@Override
 	public void onInitializeClient(){
-
+		Takeabow.LOGGER.info("Client initialization for Take A Bow");
 	}
 }
